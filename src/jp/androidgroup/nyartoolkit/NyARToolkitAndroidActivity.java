06a7a3fe1d3173e3d4d6858bc01353227029e984
zeesmall <small.a.big@gmail.com>
2012-09-23 23:26:43 +0900

    ScreenCapture function GL can be Capture.

--- src/jp/androidgroup/nyartoolkit/NyARToolkitAndroidActivity.java
+++ src/jp/androidgroup/nyartoolkit/NyARToolkitAndroidActivity.java
@@ -1,6 +1,9 @@
 package jp.androidgroup.nyartoolkit;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.nio.Buffer;
+import java.nio.IntBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
 
@@ -20,6 +23,9 @@ import jp.nyatla.nyartoolkit.markersystem.NyARMarkerSystemConfig;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.hardware.Camera;
 import android.os.Environment;
 import android.util.Log;
@@ -28,6 +34,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 
 /**
@@ -37,32 +44,46 @@ import android.widget.FrameLayout;
  */
 public class NyARToolkitAndroidActivity extends AndSketch implements AndGLView.IGLFunctionEvent
 {
+
+	// Log認識用タグ
+	private static final String TAG = "NyARToolkitAndroid";
+	
 	CameraPreview _camera_preview;
 	AndGLView _glv;
 	Camera.Size _cap_size;
-	private KGLModelData[] model_data = new KGLModelData[2];
 	
+	// マーカーの数
+	private static final int PAT_MAX = 2;
+	private KGLModelData[] model_data = new KGLModelData[PAT_MAX];
+
+	// Modelの制御
 	float lastX = 0;
 	float lastY = 0;
 	float scale = 2f;
 	float xpos=0,ypos=0,zpos=0,xrot=0,yrot=0,zrot=0;
 	int mode = 0;
 	
+	// スクリーンキャプチャ関係
+	boolean screenCapture = false;
+	boolean isGLBitmap = false;
+	Bitmap GLBitmap;
+
 	// for model renderer
 	private static final int CROP_MSG = 1;
 	private static final int FIRST_TIME_INIT = 2;
 	private static final int RESTART_PREVIEW = 3;
 	private static final int CLEAR_SCREEN_DELAY = 4;
-    private static final int SET_CAMERA_PARAMETERS_WHEN_IDLE = 5;
-    public static final int SHOW_LOADING = 6;
-    public static final int HIDE_LOADING = 7;
-    
-    static {
-    	System.loadLibrary("yuv420sp2rgb");
-    }
-    public static native void decodeYUV420SP(int[] rgb, byte[] yuv420sp, int width, int height, int type);
+	private static final int SET_CAMERA_PARAMETERS_WHEN_IDLE = 5;
+	public static final int SHOW_LOADING = 6;
+	public static final int HIDE_LOADING = 7;
+
+	// YUV420 convert RGB(naitive)
+	static {
+		System.loadLibrary("yuv420sp2rgb");
+	}
+	public static native void decodeYUV420SP(int[] rgb, byte[] yuv420sp, int width, int height, int type);
 	public static native void decodeYUV420SP(byte[] rgb, byte[] yuv420sp, int width, int heught, int type);
-	
+
 	/**
 	 * onStartでは、Viewのセットアップをしてください。
 	 */
@@ -73,7 +94,7 @@ public class NyARToolkitAndroidActivity extends AndSketch implements AndGLView.I
 		FrameLayout fr=((FrameLayout)this.findViewById(R.id.sketchLayout));
 		//カメラの取得
 		this._camera_preview=new CameraPreview(this);
-//		this._cap_size=this._camera_preview.getRecommendPreviewSize(320,240);
+		//		this._cap_size=this._camera_preview.getRecommendPreviewSize(320,240);
 		this._cap_size=this._camera_preview.getRecommendPreviewSize(640,480);
 		//画面サイズの計算
 		int h = this.getWindowManager().getDefaultDisplay().getHeight();
@@ -89,11 +110,11 @@ public class NyARToolkitAndroidActivity extends AndSketch implements AndGLView.I
 
 	NyARAndSensor _ss;
 	NyARAndMarkerSystem _ms;
-	private int[] _mid = new int[2];
+	private int[] _mid = new int[PAT_MAX];
 	AndGLTextLabel text;
 	AndGLBox box;
 	AndGLFpsLabel fps;
-	
+
 	public void setupGL(GL10 gl)
 	{
 		try
@@ -105,7 +126,7 @@ public class NyARToolkitAndroidActivity extends AndSketch implements AndGLView.I
 			this._ms=new NyARAndMarkerSystem(new NyARMarkerSystemConfig(this._cap_size.width,this._cap_size.height));
 			this._mid[0]=this._ms.addARMarker(assetMng.open("AR/data/hiro.pat"),16,25,80);
 			this._mid[1]=this._ms.addARMarker(assetMng.open("AR/data/kanji.pat"),16,25,80);
-			
+
 			try {
 				//LocalContentProvider content_provider=new LocalContentProvider("Kiageha.mqo");
 				//model_data = KGLModelData.createGLModel(gl,null,content_provider,0.015f, KGLExtensionCheck.IsExtensionSupported(gl,"GL_ARB_vertex_buffer_object"));
@@ -115,7 +136,7 @@ public class NyARToolkitAndroidActivity extends AndSketch implements AndGLView.I
 				e.printStackTrace();
 				throw new NyARException(e);
 			}
-			
+
 			this._ss.start();
 			//setup openGL Camera Frustum
 			gl.glMatrixMode(GL10.GL_PROJECTION);
@@ -125,14 +146,16 @@ public class NyARToolkitAndroidActivity extends AndSketch implements AndGLView.I
 			this._debug=new AndGLDebugDump(this._glv);
 			this.fps=new AndGLFpsLabel(this._glv,"MarkerPlaneActivity");
 			this.fps.prefix=this._cap_size.width+"x"+this._cap_size.height+":";
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			this.finish();
 		}
 	}
+	
 	AndGLDebugDump _debug=null;
+	
 	/**
 	 * 継承したクラスで表示したいものを実装してください
 	 * @param gl
@@ -152,19 +175,22 @@ public class NyARToolkitAndroidActivity extends AndSketch implements AndGLView.I
 			synchronized(this._ss){
 				this._ms.update(this._ss);
 				for(int id : _mid){
-					if(this._ms.isExistMarker(id)){
-						drawModelData(gl, id);
-					}
+					if(this._ms.isExistMarker(id)) drawModelData(gl, id);
 				}
+			}
+			// GL描写部分を画像で保存
+			if(screenCapture){
+				glScreenCapture(gl);
 			}
 		}catch(Exception e)
 		{
 			ex=e;
 		}
 	}
-	
+
+
 	/**
-	 * idのに一致するモデルを描写します
+	 * idに一致するモデルを描写します
 	 * 
 	 * @param gl
 	 * @param id
@@ -186,78 +212,78 @@ public class NyARToolkitAndroidActivity extends AndSketch implements AndGLView.I
 		model_data[id].disables(gl);
 	}
 
-    public boolean onCreateOptionsMenu(Menu menu){
-    	 
-        // メニューアイテムの追加
-        menu.add(Menu.NONE, 0, Menu.NONE, "Position");
-        menu.add(Menu.NONE, 1, Menu.NONE, "Rotate");
-        menu.add(Menu.NONE, 2, Menu.NONE, "Scale");
-        menu.add(Menu.NONE, 3, Menu.NONE, "ScreenCapture");
- 
-        return true;
-    }
-    
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	 
-        // addしたときのIDで識別
-        switch (item.getItemId()) {
-        case 0:
-            mode = 0;;
-            return true;
-     
-        case 1:
-            mode = 1;
-            return true;
-     
-        case 2:
-            mode = 2;
-            return true;
-        case 3:
-        	Shot();
-        	return true;
-        }
-        return false;
-    }
-    
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
+	public boolean onCreateOptionsMenu(Menu menu){
 
-    	Log.d("OnTouch","ontouch");
-    	switch (event.getAction()) {
-    	case MotionEvent.ACTION_DOWN:
-    		lastX = event.getX();
-    		lastY = event.getY();
-    		break;
+		// メニューアイテムの追加
+		menu.add(Menu.NONE, 0, Menu.NONE, "Position");
+		menu.add(Menu.NONE, 1, Menu.NONE, "Rotate");
+		menu.add(Menu.NONE, 2, Menu.NONE, "Scale");
+		menu.add(Menu.NONE, 3, Menu.NONE, "ScreenCapture");
 
-    	case MotionEvent.ACTION_MOVE:
-    		float dX = lastX - event.getX();
-    		float dY = lastY - event.getY();
-    		lastX = event.getX();
-    		lastY = event.getY();
+		return true;
+	}
 
-    		switch(mode){
-    		case 0 :
-    			setXpos(-dX/1.0f);
-    			setYpos(dY/1.0f);
-    			Log.d("ontatuc",xpos +"/"+  ypos);
-    			break;
-    		case 1 :
-    			setXrot(0.80f*-dY);
-    			setYrot(0.80f*-dX);
-    			Log.d("rotate",xrot +"/"+yrot);
-    			return true;
-    		case 2 :
-    			setScale(dY/10.0f);
-    			Log.d("scale",scale + "");
-    			return true;
-    		}
+	public boolean onOptionsItemSelected(MenuItem item) {
 
-    	case MotionEvent.ACTION_UP:
-    		break;
-    	}
-    	return true;
-    }
-	
+		// addしたときのIDで識別
+		switch (item.getItemId()) {
+		case 0:
+			mode = 0;;
+			return true;
+
+		case 1:
+			mode = 1;
+			return true;
+
+		case 2:
+			mode = 2;
+			return true;
+		case 3:
+			Shot();
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+
+		Log.d("OnTouch","ontouch");
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			lastX = event.getX();
+			lastY = event.getY();
+			break;
+
+		case MotionEvent.ACTION_MOVE:
+			float dX = lastX - event.getX();
+			float dY = lastY - event.getY();
+			lastX = event.getX();
+			lastY = event.getY();
+
+			switch(mode){
+			case 0 :
+				setXpos(-dX/1.0f);
+				setYpos(dY/1.0f);
+				Log.d("ontatuc",xpos +"/"+  ypos);
+				break;
+			case 1 :
+				setXrot(0.80f*-dY);
+				setYrot(0.80f*-dX);
+				Log.d("rotate",xrot +"/"+yrot);
+				return true;
+			case 2 :
+				setScale(dY/10.0f);
+				Log.d("scale",scale + "");
+				return true;
+			}
+
+		case MotionEvent.ACTION_UP:
+			break;
+		}
+		return true;
+	}
+
 	public void setScale(float f) {
 		this.scale += f;
 		if(this.scale < 0.0001f)
@@ -279,11 +305,12 @@ public class NyARToolkitAndroidActivity extends AndSketch implements AndGLView.I
 	public void setYpos(float f) {
 		this.ypos += f;
 	}
-	
+
+
 	/**
-	 * スクリーンキャプチャ<br>
-	 * takePictureでうまくいかなかったので、プレビューを加工して保存する．<br>
-	 * CGModemは写らない<br>
+	 * スクリーンキャプチャを撮る．<br>
+	 * takePictureでうまくいかなかったので、プレビューを加工してBitmpで保存する．<br>
+	 * CGModelは写らないので{@link #glScreenCapture(GL10)}を使う．<br>
 	 * 参考 : http://android.roof-balcony.com/camera/preview-get/
 	 * 
 	 * @author s0921122
@@ -291,43 +318,129 @@ public class NyARToolkitAndroidActivity extends AndSketch implements AndGLView.I
 	 * @version 1.0
 	 */
 	private void Shot(){
-		Log.d("Shot","Screen Capture Start");
+		Log.d(TAG,"Screen Capture Start");
 		int width = _cap_size.width;
 		int height = _cap_size.height;
-		Bitmap bmp = null;
-		
-		try{
-		// ARGB8888の画素の配列
-		int[] rgb = new int[(width * height)]; 
-		// ARGB8888で空のビットマップ作成
-		bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888); 
+		Bitmap cameraBitmap = null;
 
-		// YUV420からRGBに変換
-		decodeYUV420SP(rgb, _camera_preview.getCurrentBuffer(), width, height, 2);
-		// 変換した画素からビットマップにセット
-		bmp.setPixels(rgb, 0, width, 0, 0, width, height); 
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		
-		FileOutputStream fos;
 		try{
-			Log.d("Shot","FileOutput Start");
-			String path = new StringBuilder()
-	        .append(Environment.getExternalStorageDirectory().getPath()).append("/")
-	        .append("SampleMQO").append(".jpg")
-	        .toString();
+			Log.d(TAG,"Empty BMP(ARG8888) Create.");
+			// ARGB8888の画素の配列
+			int[] rgb = new int[(width * height)]; 
+			// ARGB8888で空のビットマップ作成
+			cameraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888); 
+
+			Log.d(TAG,"Decode Start.");
+			// YUV420からRGBに変換
+			decodeYUV420SP(rgb, _camera_preview.getCurrentBuffer(), width, height, 2);
+			Log.d(TAG,"Decode done.");
+			Log.d(TAG,"Camera Preview Capture Create Start.");
+			// 変換した画素からビットマップにセット
+			cameraBitmap.setPixels(rgb, 0, width, 0, 0, width, height); 
+			Log.d(TAG,"Camera Preview Capture Create done.");
 			
-			fos = new FileOutputStream(path);
-			// JPEGで保存
-			bmp.compress(CompressFormat.JPEG, 100, fos);
-			Log.d("Shot","FileOutput end. \nOutput Path : " + path);
-			
+			screenCapture = true;
+
+			while(true){
+				if(isGLBitmap) break;
+			}
+			// 画像を重ね合わせて保存する
+			overlayBMP(cameraBitmap, GLBitmap);
+
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		Log.d("Shot","ScreenCapture end.");
 	}
 
+	/**
+	 * GLで描写している部分をキャプチャする<br>
+	 * 参考 : http://www.anddev.org/how_to_get_opengl_screenshot__useful_programing_hint-t829.html
+	 * 
+	 * @author s0921122
+	 * @version 1.0
+	 * @param gl
+	 */
+	private void glScreenCapture(GL10 gl){
+		Log.d(TAG,"GL Drawing Screen Capture Start.");
+		screenCapture = false;
+		int takeWidth = _cap_size.width;
+		int takeHeight = _cap_size.height;
+		int[] tmp = new int[takeHeight*takeWidth];
+		int[] screenshot = new int[takeHeight*takeWidth];
+		Buffer screenshotBuffer = IntBuffer.wrap(tmp);
+		screenshotBuffer.position(0);
+		gl.glReadPixels(0,0,takeWidth,takeHeight, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, screenshotBuffer); 
+
+		for(int i=0; i<takeHeight; i++) 
+		{
+			//remember, that OpenGL bitmap is incompatible with Android bitmap 
+			//and so, some correction need.      
+			for(int j=0; j<takeWidth; j++) 
+			{ 
+				int pix=tmp[i*takeWidth+j]; 
+				int pb=(pix>>16)&0xff; 
+				int pr=(pix<<16)&0x00ff0000; 
+				int pix1=(pix&0xff00ff00) | pr | pb; 
+				screenshot[(takeHeight-i-1)*takeWidth+j]=pix1; 
+			} 
+		} 
+		// アルファありのBitmapを作成する
+		this.GLBitmap = Bitmap.createBitmap(screenshot, takeWidth, takeHeight, Config.ARGB_8888);
+		Log.d(TAG,"GL Drawing Screen Capture. Create done.");
+		// GLをキャプチャしたBitmapを作成したのでフラグを立てる
+		isGLBitmap = true;
+	}
+	
+
+	/**
+	 * 2枚のBitmap重ね合わせて保存する．<br>
+	 * このプログラムではカメラとGLのBitmapを重ね合わせて保存する．
+	 * 
+	 * @param under 背景になる画像
+	 * @param upper 上に乗る画像
+	 */
+	private void overlayBMP(Bitmap under,Bitmap upper){
+		Log.d(TAG,"Bitmap Ovelray Start.");
+		//ARGB_8888,RGB_565,ARGB_4444
+		int width = under.getWidth();
+		int height = under.getHeight();
+
+		Log.d(TAG,"Create Base Bitmap.");
+		// 合成するための下地
+		Bitmap newBitmap = Bitmap.createBitmap(width, height,Bitmap.Config.RGB_565);
+
+		Log.d(TAG,"Create Canvas.");
+		Canvas canvas = new Canvas(newBitmap);
+		Log.d(TAG,"Overlay Start.");
+		canvas.drawBitmap(under, 0, 0, (Paint)null);
+		canvas.drawBitmap(upper, 0, 0, (Paint)null);
+		Log.d(TAG,"Overlay done.");
+		Log.d(TAG,"Recycle Start.");
+		upper.recycle();
+		under.recycle();
+		GLBitmap.recycle();
+		isGLBitmap = false;
+		Log.d(TAG,"Recycle done.");
+
+		FileOutputStream fos;
+		try{
+			Log.d(TAG,"FileOutput Start.");
+			String path = new StringBuilder()
+			.append(Environment.getExternalStorageDirectory().getPath()).append("/")
+			.append("SampleMQO").append(".jpg")
+			.toString();
+
+			fos = new FileOutputStream(path);
+			// JPEGで保存
+			newBitmap.compress(CompressFormat.JPEG, 100, fos);
+			Toast.makeText(this, "ScreenCapture Success.", Toast.LENGTH_LONG).show();
+			Log.d(TAG,"FileOutput end. \nOutput Path : " + path);
+			Log.d(TAG,"ScreenCapture end.");
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	Exception ex=null;
 }
